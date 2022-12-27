a95ec01481ba47c072a397da542b94bc4b4c0ecb
galkowskim <mikolaj.galkowski7@gmail.com>
2022-12-27 19:25:08 +0100

    implement basic crud operations

---
+++ todolist/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    status_of_completion = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        order_with_respect_to = 'status_of_completion'
